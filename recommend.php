60e5515f60c96079419388bcb3a1503b66ab1762
hanzzb <hellojione2@gmail.com>
2020-06-19 02:41:09 +0900

    recommend 1차 완성
    추천페이지를 다 만들었습니다 recommend 브랜치를 만들어봤습니다 브랜치 머지는 성공할지 모르겠네용..

--- recommend.php
+++ recommend.php
@@ -1,25 +1,52 @@
+<center>
+<br><br><br>
+
 
 <select id="select_age" name="age" >
-  <option value="0">선택</option>
-  <option value="1">20대</option>
-  <option value="2">30대</option>
-  <option value="3">40대이상</option>
+  <option value="선택1">선택</option>
+  <option value="20">20대</option>
+  <option value="30">30대</option>
+  <option value="40">40대이상</option>
 </select>
 
 <select id="select_price" name="price" >
-  <option value="0">선택</option>
-  <option value="1">1-2만원대</option>
-  <option value="2">2-3만원대</option>
-  <option value="3">3-4만원대</option>
-  <option value="4">4만원이상</option>
+  <option value="선택2">선택</option>
+  <option value="1-2만원대">1-2만원대</option>
+  <option value="2-3만원대">2-3만원대</option>
+  <option value="3-4만원대">3-4만원대</option>
+  <option value="4만원이상">4만원이상</option>
 </select>
 
 <select id="select_mood" name="mood" >
-  <option value="0">선택</option>
-  <option value="1">데이트</option>
-  <option value="2">회식</option>
-  <option value="3">가족모임</option>
-  <option value="4">이국적</option>
-  <option value="5">캐주얼한</option>
-  <option value="6">고급스러운</option>
+  <option value="선택3">선택</option>
+  <option value="데이트">데이트</option>
+  <option value="회식">회식</option>
+  <option value="가족모임">가족모임</option>
+  <option value="이국적">이국적</option>
+  <option value="캐주얼한">캐주얼한</option>
+  <option value="고급스러운">고급스러운</option>
 </select>
+<button onclick="reco()">찾아주세요!</button>
+
+<center>
+  <p id="result"></p>
+  <script src="//code.jquery.com/jquery.min.js"></script>
+  <script>
+
+      function reco() {
+        $.ajax({
+            url: "recommend_result.php",
+            type: "get",
+            dataType: "html",
+            data: {
+                select_age: $('#select_age').val(),
+                select_price: $('#select_price').val(),
+                select_mood: $('#select_mood').val(),
+            }
+        }).done(function(data) {
+            $('#result').html(data);
+        });
+      }
+
+
+  </script>
