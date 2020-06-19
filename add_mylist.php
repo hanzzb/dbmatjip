15d4d4e41c4d99e834264f06661af0121f95fa37
cherry2030 <cherry2030@naver.com>
2020-06-20 08:58:12 +0900

    리뷰 작성기능 추가 및 수정

--- add_mylist.php
+++ add_mylist.php
@@ -4,9 +4,19 @@
 
   $add_restaurant_id = $_POST["add_restaurant_id"];
 
-   $mylist_query = "INSERT INTO `mylist`(`member_id`, `restaurant_id`) VALUES ('{$_SESSION["user_id"]}','{$add_restaurant_id}')";
-   mysqli_query($conn, $mylist_query);
+  if (!isset($_SESSION['user_id'])){
+    echo "<script>
+    alert('로그인을 해야 사용 가능합니다!');
+    location.href='restaurant_detail_info.php?restaurant_id=$add_restaurant_id';
+    </script>";
+  }
+  else{
+    $mylist_query = "INSERT INTO `mylist`(`member_id`, `restaurant_id`) VALUES ('{$_SESSION["user_id"]}','{$add_restaurant_id}')";
+       mysqli_query($conn, $mylist_query);
 
-   echo "<script>alert('추가되었습니다!');
-   location.href='./main.php';</script>"
+       echo "<script>alert('추가되었습니다!');
+       location.href='./main.php';</script>";
+  }
+
+
 ?>
--- change_pw.php
+++ change_pw.php
@@ -2,10 +2,8 @@
   session_start();
   include './dbconn.php';
 
-  session_start();
   if (isset($_SESSION['user_id'])){
     $user_id = $_SESSION['user_id'];
-
   }
   else {
     echo " again3~";
@@ -13,8 +11,6 @@
 
   $new_pw = $_POST["user_pw"];
   //$review_word = $_POST["review"];
-
-
 
    $update_password_query = "UPDATE member SET email='$new_pw' where member_id = '$user_id'";
    mysqli_query($conn, $update_password_query);
