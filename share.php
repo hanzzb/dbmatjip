45dddd249b23ffb18dddfdc9bacaf5b208cf9e55
hanzzb <hellojione2@gmail.com>
2020-06-20 09:01:16 +0900

    이메일 보내고나서 원래 페이지로 돌아가기

--- share.php
+++ share.php
@@ -9,7 +9,6 @@
   $result = mysqli_query($conn, $query);
   $num = mysqli_num_rows($result);
 
-echo "$restaurant_id";
 
   if($num)
   {
@@ -19,15 +18,27 @@ echo "$restaurant_id";
     $query2="SELECT * from restaurant where restaurant_id ='$restaurant_id'";
     $result2 = mysqli_query($conn, $query2);
     $row2 = mysqli_fetch_array($result2);
-    echo"$row2[1]";
+
 
 
     echo "
 
     <center>
-    <a href='https://mail.google.com/mail/?view=cm&fs=1&to=$row[2]&su=[밥은 먹고 다니지?]친구야 나랑 같이 맛집가자~&body=$row2[1] 어때? 아래 링크로 들어가서 확인해봐! %0D%0Ahttp://localhost/project/restaurant_detail_info.php?restaurant_id=$restaurant_id' target='_blank' >메일 보내기</a>
+    <br>
+    <a href='https://mail.google.com/mail/?view=cm&fs=1&to=$row[2]&su=[밥은 먹고 다니지?]친구야 나랑 같이 맛집가자~&body=$row2[1] 어때? 아래 링크로 들어가서 확인해봐! %0D%0Ahttp://localhost/project/restaurant_detail_info.php?restaurant_id=$restaurant_id'
+    onclick= 'plz()' target='_blank' >메일 보내기</a>
     </center>
 
+    ";
+
+    echo "
+    <script>
+    function plz()
+    {
+      alert('메일창으로 이동합니다');
+      location.href='restaurant_detail_info.php';
+    }
+    </script>
     ";
 
 
