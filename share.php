41adb2470ecb8580f088fb6539acc5149657d71a
hanzzb <hellojione2@gmail.com>
2020-06-21 15:11:37 +0900

    메일 양식 수정, 추천 selectbox 수정

--- share.php
+++ share.php
@@ -16,7 +16,10 @@
 
     $row = mysqli_fetch_array($result);
 
-    $query2="SELECT * from restaurant where restaurant_id ='$restaurant_id'";
+    //$query2="SELECT * from restaurant where restaurant_id ='$restaurant_id'";
+    $query2="SELECT * from restaurant
+            LEFT JOIN restaurant_info ON restaurant.restaurant_id = restaurant_info.restaurant_id
+            WHERE restaurant.restaurant_id ='$restaurant_id'";
     $result2 = mysqli_query($conn, $query2);
     $row2 = mysqli_fetch_array($result2);
 
@@ -26,8 +29,11 @@
 
     <center>
     <br>
-    <a href='https://mail.google.com/mail/?view=cm&fs=1&to=$row[2]&su=[밥은 먹고 다니지?]친구야 나랑 같이 맛집가자~&body=$row2[1] 어때? 아래 링크로 들어가서 확인해봐! %0D%0Ahttp://localhost/project/restaurant_detail_info.php?restaurant_id=$restaurant_id'
+
+
+    <a href='https://mail.google.com/mail/?view=cm&fs=1&to=$row[2]&su=[밥은 먹고 다니지?]친구야 나랑 같이 맛집가자~&body=$row2[1] 어때? %0D%0A%0D%0A-----------정보-----------%0D%0A주소 : $row2[2] $row2[6] %0D%0A종류 : $row2[3] %0D%0A별점 : $row2[8]%0D%0A--------------------------'
     onclick= 'plz()' target='_blank' >메일 보내기</a>
+
     </center>
 
     ";
